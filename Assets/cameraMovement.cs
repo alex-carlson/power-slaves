4231d9adc5d2149915e609306308140f1029ec6b
alex-carlson <alex@acwd.me>
2016-07-02 22:54:51 -0500

    enemy basic AI and movement

--- Assets/cameraMovement.cs
+++ Assets/cameraMovement.cs
@@ -4,14 +4,25 @@ using System.Collections;
 public class cameraMovement : MonoBehaviour {
 
 	public GameObject player;
+	Camera m_Cam;
+	float dist = 0;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start(){
+		m_Cam = GetComponent<Camera> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 p = m_Cam.ScreenToWorldPoint (new Vector3 (Screen.width / 2, Screen.height / 2, m_Cam.nearClipPlane));
+
+		if (player.transform.position.x > p.x + 64) {
+			m_Cam.transform.position += (Vector3.right * 128);
+		} else if (player.transform.position.x < p.x - 64) {
+			m_Cam.transform.position += (-Vector3.right * 128);
+		} else if (player.transform.position.y > p.y + 64) {
+			m_Cam.transform.position += (Vector3.up * 128);
+		} else if (player.transform.position.y < p.y - 64) {
+			m_Cam.transform.position += (-Vector3.up * 128);
+		}
 	}
 }
--- Assets/enemyAI.cs
+++ Assets/enemyAI.cs
@@ -3,20 +3,51 @@ using System.Collections;
 
 public class enemyAI : MonoBehaviour {
 
+	[Range(0, 1)] public float Speed = 0.1f;
+	enum Direction {North, South, East, West, None};
+
+	Direction myDirection;
+
 	GameObject plr;
 	float dist;
+	bool inView = true;
 
 	// Use this for initialization
 	void Start () {
 		plr = GameObject.FindGameObjectWithTag ("Player");
+		InvokeRepeating ("Brain", 2, 2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		dist = Vector3.Distance (transform.position, plr.transform.position);
 
-		if (dist < 30f) {
-			transform.Translate (Vector3.MoveTowards(transform.position, plr.transform.position, 0.0001f));
+		if (dist < 10 && inView == true) {
+			//chase
+			transform.position = Vector3.MoveTowards(transform.position, plr.transform.position, Speed  * Time.deltaTime);
+
+		} else {
+			if (myDirection == Direction.North) {
+				transform.Translate (Vector3.up * Speed);
+			} else if (myDirection == Direction.East) {
+				transform.localRotation = Quaternion.Euler(0, 0, 0);
+				transform.Translate (Vector3.right * Speed);
+			} else if (myDirection == Direction.South) {
+				transform.Translate (-Vector3.up * Speed);
+			} else if(myDirection == Direction.West){
+				transform.Translate (Vector3.right * Speed);
+				transform.localRotation = Quaternion.Euler(0, 180, 0);
+			}
+		}
+	}
+
+	void Brain(){
+		myDirection = (Direction)Random.Range (0, 4);
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.transform.tag != "Player") {
+			myDirection = Direction.None;
 		}
 	}
 }
